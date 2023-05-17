fdddb0c5cc89c7d5124f37aef87561984a448964
Nikita <a2ntchweb@gmail.com>
2023-05-17 14:27:39 +0300

    Create seeder for inserting one string

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,24 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('donations')->insert([
+            'donatorname' => Str::random(8),
+            'email' => Str::random(10) . '@gmail.com',
+            'amount' => mt_rand(1, 50),
+            'message' => Str::random(20),
+            'date' => '2023-05-17 00:00:00'
+        ]);
     }
 }
