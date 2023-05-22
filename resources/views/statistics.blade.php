0adfd157cb10b860634f213cd33b0973dd51eb79
Nikita <a2ntchweb@gmail.com>
2023-05-22 14:32:25 +0300

    Add template with a list of all donations

--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -15,5 +15,47 @@
             You can see donation statistics for the last month below.
         </p>
     </div>
+
+    <div class="table-responsive">
+        <h2>List of all donations</h2>
+        <table class="table table-striped table-sm">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Donator Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Message</th>
+                    <th scope="col">Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1,001</td>
+                    <td>random</td>
+                    <td>data</td>
+                    <td>placeholder</td>
+                    <td>text</td>
+                    <td>date</td>
+                </tr>
+                <tr>
+                    <td>1,002</td>
+                    <td>placeholder</td>
+                    <td>irrelevant</td>
+                    <td>visual</td>
+                    <td>layout</td>
+                    <td>date</td>
+                </tr>
+                <tr>
+                    <td>1,003</td>
+                    <td>data</td>
+                    <td>rich</td>
+                    <td>dashboard</td>
+                    <td>tabular</td>
+                    <td>date</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
