1e15817cc7423417a9472a881d8de28c60b83114
Nikita <a2ntchweb@gmail.com>
2023-05-22 11:48:15 +0300

    Create home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Home Page</title>
+</head>
+<body>
+    <div class="grid-container">
+        <div class="home-page-content">
+            <h1>Donations</h1>
+            <p>
+                We provide an opportunity to make donations. 
+            </p>
+            <div class="refs-container">
+                <a href="form">Form</a>
+                <a href="chart">Chart</a>
+                <a href="table">Table</a>
+            </div>
+        </div>
+        <div class="main-content">
+
+        </div>
+    </div>
+</body>
+</html>
